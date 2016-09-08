537e7327948ba91e06fc5592e0e4b9259b9a7976
Paul Bevis <paulbevis2010@gmail.com>
2016-09-08 21:00:55 +1000

    Linear gradient for percentage code coverage

--- js/components/code-coverage.js
+++ js/components/code-coverage.js
@@ -3,6 +3,21 @@ import {propEq, filter} from 'ramda'
 
 export default class CodeCoverage extends React.Component {
 
+  buildStyling(level) {
+    var obj = {
+      padding: '5px 0',
+      margin: '0 5px'
+    };
+
+    obj.backgroundColor = 'gray';
+    obj.backgroundImage = `
+    linear-gradient(
+      to right,
+      lightGray ${level}%,
+      gray ${100 - Number(level)}%)`;
+    return obj;
+  }
+
   render() {
     let topLevelStyle = {
       display: 'flex',
@@ -18,13 +33,18 @@ export default class CodeCoverage extends React.Component {
       justifyContent: 'space-between',
       borderRadius: '5px',
       border: '4px solid gray',
-      margin: '0 5px'
+      margin: '0 5px',
+      backgroundColor: 'gray'
     };
 
     let styling = {
       padding: '5px 5px',
-      backgroundColor: 'gray'
+      backgroundColor: 'gray',
     };
+
+    let stylingPercentFuncs = this.buildStyling(this.props.functions);
+    let stylingPercentStmts = this.buildStyling(this.props.statements);
+    let stylingPercentLines = this.buildStyling(this.props.lines);
 
     return (
 
@@ -32,15 +52,15 @@ export default class CodeCoverage extends React.Component {
 
         <div style={wrappingStyle}>
           <div style={styling}>Lines:</div>
-          <div style={styling}>{this.props.lines}</div>
+          <div style={stylingPercentLines}>{this.props.lines}%</div>
         </div>
         <div style={wrappingStyle}>
           <div style={styling}>Funcs:</div>
-          <div style={styling}>{this.props.functions}</div>
+          <div style={stylingPercentFuncs}>{this.props.functions}%</div>
         </div>
         <div style={wrappingStyle}>
           <div style={styling}>Stmts:</div>
-          <div style={styling}> {this.props.statements}</div>
+          <div style={stylingPercentStmts}> {this.props.statements}%</div>
         </div>
       </div>
     )
